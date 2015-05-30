8245c7790b78d35ac220ebbfefaf70856fe6efd9
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-05-30 09:18:04 +0200

    Routing and controllers

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,47 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.order("created_at desc").all
+  end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)
+    if @recipe.save
+      flash[:success] = "You have successfully added a new recipe"
+      redirect_to recipe_path(@recipe)
+    else
+      flash.now[:danger] = "You have some errors"
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was update successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      flash.now[:error] = "There was an error"
+      render :edit
+    end
+  end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :summary, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/home' => 'static_pages#home'
+
+  resources :recipes
 end
