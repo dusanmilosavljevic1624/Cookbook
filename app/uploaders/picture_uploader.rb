3150134cc83484116452e708b38f4251f26f4178
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-06-01 00:31:38 +0200

    Minor changes

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,11 +6,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #if Rails.env.production?
+  #  storage :fog
+  #else
+  #  storage :file
+  #end
+
+  storage :file #until i succesfully set up s3
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
